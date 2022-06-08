b83aa1e50a95807ba90771d8c86277b260d88b9e
gloriablink <82380576+gloriablink@users.noreply.github.com>
2022-06-08 17:36:12 +0200

    Update style.js

--- style.js
+++ style.js
@@ -25,8 +25,10 @@
 				{ name: 'Emphasis 3', element: 'span', attributes: { 'class': 'bck-enfasis-3'} },
 				{ name: 'Emphasis 4', element: 'span', attributes: { 'class': 'bck-enfasis-4'} },
 
-				{ name: 'Ordered list 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-1' } },
+				{ name: 'Ordered list 1 MC', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-1' } },
 				{ name: 'Ordered list 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
+				{ name: 'Ordered list 3 MC', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
+				{ name: 'Ordered list 4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
 
 				{ name: 'Unordered list 1', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-1' } },
 				{ name: 'Unordered list 2', element: 'ul', attributes: { 'class': 'bck-ul bck-ul-2' } },
